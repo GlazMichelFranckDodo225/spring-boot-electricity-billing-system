49aaeaf71b3a914795ca0c8a77dfe14bf0bdab8b
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-01-28 08:55:19 +0100

    Project Initialization

---
+++ src/test/java/com/dgmf/SpringBootElectricityBillingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootElectricityBillingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
